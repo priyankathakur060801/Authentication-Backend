65dca9d04f3f8308b465753e02babe0b8c34ca4f
priyankathakur06 <priyankathakur060801@gmail.com>
2025-09-20 12:20:13 +0530

    cookie set

--- controllers/authController.js
+++ controllers/authController.js
@@ -75,7 +75,7 @@ export const login = async(req,res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      sameSite: process.env.NODE_ENV1 === "production" ? "none" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
    
@@ -94,7 +94,7 @@ export const logout = (req,res) => {
     res.clearCookie("token", {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      sameSite: process.env.NODE_ENV1 === "production" ? "none" : "strict",
     });
 
     return res.json({ success: true, message: "Logged out" });
