b26a36030fca17785bd504ff76ac578d2dc7774d
priyankathakur06 <priyankathakur060801@gmail.com>
2025-09-20 12:17:54 +0530

    cookie set

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
-  "https://auth-frontend-app.vercel.app",
+  "https://auth-frontend-app.vercel.app"
 ];
 
 app.use(
