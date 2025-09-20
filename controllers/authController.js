df502ce9d1952a22d9cbc1b4ea84b4facf20cea5
priyankathakur06 <priyankathakur060801@gmail.com>
2025-09-20 12:07:20 +0530

    cookie set

--- controllers/authController.js
+++ controllers/authController.js
@@ -29,7 +29,7 @@ export const register = async(req,res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV ==='production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            sameSite: process.env.NODE_ENV1 === 'production' ? 'none' : 'strict',
             maxAge:7*24*60*60*1000
         })
       const mailOptions = {
